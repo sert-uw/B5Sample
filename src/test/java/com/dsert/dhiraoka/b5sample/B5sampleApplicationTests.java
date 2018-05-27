96ce2ac0ad9a304660c168869cf4cffb5bf58321
dhiraoka <demo.sert@gmail.com>
2018-05-27 11:18:24 +0900

    テストコード実装

--- src/test/java/com/dsert/dhiraoka/b5sample/B5sampleApplicationTests.java
+++ src/test/java/com/dsert/dhiraoka/b5sample/B5sampleApplicationTests.java
@@ -1,16 +1,36 @@
 package com.dsert.dhiraoka.b5sample;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class B5sampleApplicationTests {
 
+    private MockMvc mvc;
+
+    @Before
+    public void before() throws Exception {
+        mvc = MockMvcBuilders.standaloneSetup(new B5sampleApplication()).build();
+    }
+
     @Test
     public void contextLoads() {
     }
 
+    @Test
+    public void indexTest() throws Exception {
+        mvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.message").value("Hello World!"));
+    }
 }
